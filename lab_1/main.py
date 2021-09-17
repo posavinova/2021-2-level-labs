dddb9e3483539acd95e4372e93fd9af96775b451
Polina Savinova <posavinova@gmail.com>
2021-09-17 17:07:25 +0300

    Review-ready version

--- lab_1/main.py
+++ lab_1/main.py
@@ -4,12 +4,14 @@ Language detection
 """
 
 
+import json
 import re
-# import json
-# from typing import Optional
+
+from os.path import exists
+from typing import Optional
 
 
-def tokenize(text: str) -> list or None:
+def tokenize(text: str) -> Optional[list]:
     """
     Splits a text into tokens, converts the tokens into lowercase,
     removes punctuation and other symbols from words
@@ -17,65 +19,79 @@ def tokenize(text: str) -> list or None:
     :return: a list of lower-cased tokens without punctuation
     """
     if isinstance(text, str):
-        return re.sub(r"[-\d,.!?'_@#№$:;\"\\]+", "", text).strip().lower().split()
+        tokenized = (
+            "".join([char for char in text if char.isalpha() or char.isspace()])
+            .strip()
+            .lower()
+            .split()
+        )
+        return tokenized
     else:
         return None
 
 
-def remove_stop_words(tokens: list, stop_words: list) -> list or None:
+def remove_stop_words(tokens: list, stop_words: list) -> Optional[list]:
     """
     Removes stop words
     :param tokens: a list of tokens
     :param stop_words: a list of stop words
     :return: a list of tokens without stop words
     """
-    if isinstance(tokens, list):
-        if isinstance(stop_words, list):
-            return [tokens.remove(word) for word in tokens if word in stop_words]
-        else:
-            return tokens
+    if (
+        isinstance(tokens, list)
+        and isinstance(stop_words, list)
+        and all(isinstance(i, str) for i in tokens)
+    ):
+        cleaned = [word for word in tokens if word not in stop_words]
+        return cleaned
     else:
         return None
 
 
-def calculate_frequencies(tokens: list) -> dict or None:
+def calculate_frequencies(tokens: list) -> Optional[dict]:
     """
     Calculates frequencies of given tokens
     :param tokens: a list of tokens
     :return: a dictionary with frequencies
     """
-    if isinstance(tokens, list):
+    if isinstance(tokens, list) and all(isinstance(i, str) for i in tokens):
         freq_dict = {}
         for word in tokens:
-            if word in freq_dict:
-                freq_dict[word] += 1
-            else:
-                freq_dict[word] = 1
+            freq_dict[word] = tokens.count(word)
         return freq_dict
     else:
         return None
 
 
-def get_top_n_words(freq_dict: dict, top_n: int) -> list or None:
+def get_top_n_words(freq_dict: dict, top_n: int) -> Optional[list]:
     """
     Returns the most common words
     :param freq_dict: a dictionary with frequencies
     :param top_n: a number of the most common words
     :return: a list of the most common words
     """
-    if isinstance(freq_dict, dict) and isinstance(top_n, int):
-        sorted_by_key = dict(sorted(freq_dict.items()))
-        sorted_by_value = dict(sorted(sorted_by_key.items(), key=lambda kv: kv[1], reverse=True))
-        tokens = list(sorted_by_value.keys())
-        if top_n <= len(tokens):
-            return tokens[:top_n]
+    if (
+        isinstance(freq_dict, dict)
+        and isinstance(top_n, int)
+        and all(isinstance(value, int) for value in freq_dict.values())
+    ):
+        sorted_dict = [
+            pair[0]
+            for pair in sorted(
+                freq_dict.items(), key=lambda item: item[1], reverse=True
+            )
+        ]
+        if top_n <= len(sorted_dict):
+            return sorted_dict[:top_n]
         else:
-            return tokens
+            return sorted_dict
     else:
         return None
 
 
-def create_language_profile(language: str, text: str, stop_words: list) -> dict or None:
+def create_language_profile(
+    language: str, text: str, stop_words: list
+) -> Optional[dict]:
     """
     Creates a language profile
     :param language: a language
@@ -83,7 +99,11 @@ def create_language_profile(language: str, text: str, stop_words: list) -> dict 
     :param stop_words: a list of stop words
     :return: a dictionary with three keys – name, freq, n_words
     """
-    if isinstance(language, str) and isinstance(text, str) and isinstance(stop_words, list):
+    if (
+        isinstance(language, str)
+        and isinstance(text, str)
+        and isinstance(stop_words, list)
+    ):
         tokenized = tokenize(text)
         cleared = remove_stop_words(tokenized, stop_words)
         freq_dict = calculate_frequencies(cleared)
@@ -93,7 +113,9 @@ def create_language_profile(language: str, text: str, stop_words: list) -> dict 
         return None
 
 
-def compare_profiles(unknown_profile: dict, profile_to_compare: dict, top_n: int) -> float or None:
+def compare_profiles(
+    unknown_profile: dict, profile_to_compare: dict, top_n: int
+) -> Optional[float]:
     """
     Compares profiles and calculates the distance using top n words
     :param unknown_profile: a dictionary
@@ -101,17 +123,23 @@ def compare_profiles(unknown_profile: dict, profile_to_compare: dict, top_n: int
     :param top_n: a number of the most common words
     :return: the distance
     """
-    if isinstance(profile_1, dict) and isinstance(profile_2, dict) and isinstance(top_n, int):
-        top_1 = get_top_n_words(profile_1, top_n)
-        top_2 = get_top_n_words(profile_2, top_n)
-        intersection = []
-        [intersection.append(word) for word in profile_1 if word in profile_2]
-        return len(intersection) / top_n
+    if (
+        isinstance(unknown_profile, dict)
+        and isinstance(profile_to_compare, dict)
+        and isinstance(top_n, int)
+    ):
+        top_1 = get_top_n_words(unknown_profile.get("freq"), top_n)
+        top_2 = get_top_n_words(profile_to_compare.get("freq"), top_n)
+        if (top_1 and top_2) is not None:
+            common = set(top_1).intersection(set(top_2))
+            return round(len(common) / len(top_2), 2)
     else:
         return None
 
 
-def detect_language(unknown_profile: dict, profile_1: dict, profile_2: dict, top_n: int) -> str or None:
+def detect_language(
+    unknown_profile: dict, profile_1: dict, profile_2: dict, top_n: int
+) -> Optional[str]:
     """
     Detects the language of an unknown profile
     :param unknown_profile: a dictionary
@@ -120,18 +148,22 @@ def detect_language(unknown_profile: dict, profile_1: dict, profile_2: dict, top
     :param top_n: a number of the most common words
     :return: a language
     """
-    if isinstance(unknown_profile, dict) and isinstance(profile_1, dict) and isinstance(profile_2, dict):
-        match_1 = calculate_distance(profile_1, unknown_profile, top_n)
-        match_2 = calculate_distance(profile_2, unknown_profile, top_n)
-        if match_1 > match_2:
-            return profile_1["name"]
-        else:
-            return profile_2["name"]
+    if (
+        isinstance(unknown_profile, dict)
+        and isinstance(profile_1, dict)
+        and isinstance(profile_2, dict)
+    ):
+        match_1 = compare_profiles(profile_1, unknown_profile, top_n)
+        match_2 = compare_profiles(profile_2, unknown_profile, top_n)
+        match = max([(match_1, profile_1["name"]), (match_2, profile_2["name"])])[1]
+        return match
     else:
         return None
 
 
-def compare_profiles_advanced(unknown_profile: dict, profile_to_compare: dict, top_n: int) -> list or None:
+def compare_profiles_advanced(
+    unknown_profile: dict, profile_to_compare: dict, top_n: int
+) -> Optional[dict]:
     """
     Compares profiles and calculates some advanced parameters
     :param unknown_profile: a dictionary
@@ -140,10 +172,38 @@ def compare_profiles_advanced(unknown_profile: dict, profile_to_compare: dict, t
     :return: a dictionary with 7 keys – name, score, common, sorted_common, max_length_word,
     min_length_word, average_token_length
     """
-    pass
+    if (
+        isinstance(unknown_profile, dict)
+        and isinstance(profile_to_compare, dict)
+        and isinstance(top_n, int)
+    ):
+        unk_top = get_top_n_words(unknown_profile.get("freq"), top_n)
+        check_top = get_top_n_words(profile_to_compare.get("freq"), top_n)
+        if (unk_top and check_top) is not None:
+            common = [token for token in check_top if token in unk_top]
+            score = len(common) / len(check_top)
+            sorted_common = sorted(common)
+            tokens = profile_to_compare["freq"].keys()
+            max_length_word = max(tokens, key=len)
+            min_length_word = min(tokens, key=len)
+            average_token_length = sum(map(len, tokens)) / len(tokens)
+            comparison = {
+                "name": profile_to_compare.get("name"),
+                "common": common,
+                "score": score,
+                "max_length_word": max_length_word,
+                "min_length_word": min_length_word,
+                "average_token_length": average_token_length,
+                "sorted_common": sorted_common,
+            }
+            return comparison
+    else:
+        return None
 
 
-def detect_language_advanced(unknown_profile: dict, profiles: list, languages: list, top_n: int) -> str or None:
+def detect_language_advanced(
+    unknown_profile: dict, profiles: list, languages: list, top_n: int
+) -> Optional[str]:
     """
     Detects the language of an unknown profile within the list of possible languages
     :param unknown_profile: a dictionary
@@ -152,16 +212,48 @@ def detect_language_advanced(unknown_profile: dict, profiles: list, languages: l
     :param top_n: a number of the most common words
     :return: a language
     """
-    pass
+    if (
+        isinstance(unknown_profile, dict)
+        and isinstance(profiles, list)
+        and isinstance(languages, list)
+        and isinstance(top_n, int)
+    ):
+        lang_match = [
+            compare_profiles_advanced(unknown_profile, profile, top_n)
+            for profile in profiles
+            if profile["name"] in languages or not languages
+        ]
+        match = sorted(
+            sorted(lang_match, key=lambda profile: profile["name"]),
+            key=lambda profile: profile["score"],
+            reverse=True,
+        )
+        if match:
+            return match[0]["name"]
+        else:
+            return None
+    else:
+        return None
 
 
-def load_profile(path_to_file: str) -> dict or None:
+def load_profile(path_to_file: str) -> Optional[dict]:
     """
     Loads a language profile
     :param path_to_file: a path
     :return: a dictionary with three keys – name, freq, n_words
     """
-    pass
+    if isinstance(path_to_file, str):
+        if exists(path_to_file):
+            with open(path_to_file, encoding="utf-8") as profile:
+                file = json.load(profile)
+                if file:
+                    return file
+                else:
+                    return None
+        else:
+            return None
+    else:
+        return None
 
 
 def save_profile(profile: dict) -> int:
@@ -170,4 +262,21 @@ def save_profile(profile: dict) -> int:
     :param profile: a dictionary
     :return: 0 if everything is ok, 1 if not
     """
-    pass
+    if isinstance(profile, dict):
+        pass
+    else:
+        return 1
+    if (
+        "name" in profile.keys()
+        and "freq" in profile.keys()
+        and "n_words" in profile.keys()
+        and isinstance(profile["name"], str)
+        and isinstance(profile["freq"], dict)
+        and isinstance(profile["n_words"], int)
+    ):
+        path_to_file = f'{profile["name"]}.json'
+        with open(path_to_file, mode="w", encoding="utf-8") as file:
+            json.dump(profile, file)
+            return 0
+    else:
+        return 1
--- lab_1/start.py
+++ lab_1/start.py
@@ -3,23 +3,65 @@ Language detection starter
 """
 
 import os
+import main
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
-PATH_TO_TEXTS_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'texts')
+PATH_TO_TEXTS_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, "texts")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'en.txt'), 'r', encoding='utf-8') as file_to_read:
+    with open(
+        os.path.join(PATH_TO_TEXTS_FOLDER, "en.txt"), "r", encoding="utf-8"
+    ) as file_to_read:
         en_text = file_to_read.read()
 
-    with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'de.txt'), 'r', encoding='utf-8') as file_to_read:
+    with open(
+        os.path.join(PATH_TO_TEXTS_FOLDER, "de.txt"), "r", encoding="utf-8"
+    ) as file_to_read:
         de_text = file_to_read.read()
 
-    with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'unknown.txt'), 'r', encoding='utf-8') as \
-            file_to_read:
+    with open(
+        os.path.join(PATH_TO_TEXTS_FOLDER, "la.txt"), "r", encoding="utf-8"
+    ) as file_to_read:
+        la_text = file_to_read.read()
+
+    with open(
+        os.path.join(PATH_TO_TEXTS_FOLDER, "unknown.txt"), "r", encoding="utf-8"
+    ) as file_to_read:
         unknown_text = file_to_read.read()
 
-    EXPECTED = 'en'
-    RESULT = ''
+    unk_profile_test = main.create_language_profile("unk", unknown_text, [])
+    main.save_profile(unk_profile_test)
+    en_profile_test = main.create_language_profile("en", en_text, ["a", "an", "the"])
+    de_profile_test = main.create_language_profile("de", de_text, [])
+    la_profile_test = main.create_language_profile("la", la_text, [])
+
+    detection_from_scratch = main.detect_language_advanced(
+        unk_profile_test, [de_profile_test, la_profile_test], ["la", "de"], 10
+    )
+
+    de_profile = main.load_profile(
+        "/home/polina/PycharmProjects/2021-2-level-labs/lab_1/profiles/de.json"
+    )
+    en_profile = main.load_profile(
+        "/home/polina/PycharmProjects/2021-2-level-labs/lab_1/profiles/en.json"
+    )
+    la_profile = main.load_profile(
+        "/home/polina/PycharmProjects/2021-2-level-labs/lab_1/profiles/la.json"
+    )
+
+    detection_from_profiles = main.detect_language_advanced(
+        unk_profile_test, [de_profile, en_profile, la_profile], ["la", "de"], 10
+    )
+
+    print(
+        f"1. Detecting language from scratch: it is most likely to be {detection_from_scratch}."
+    )
+    print(
+        f"2. Detecting language from profiles: it is likely to be {detection_from_profiles}."
+    )
+
+    EXPECTED = "en"
+    RESULT = detection_from_profiles
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT, 'Detection not working'
+    assert RESULT, "Detection not working"
