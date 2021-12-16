fc383841e9d48e550400a261faaf18552971a4b1
posavinova <posavinova@gmail.com>
2021-12-16 22:39:26 +0300

    Final changes

--- lab_4/main.py
+++ lab_4/main.py
@@ -2,10 +2,12 @@
 Lab 4
 Language generation algorithm based on language profiles
 """
-
+import re
+import json
 from typing import Tuple
 from lab_4.storage import Storage
 from lab_4.language_profile import LanguageProfile
+from lab_4.language_profile import NGramTrie
 
 
 # 4
@@ -15,15 +17,9 @@ def tokenize_by_letters(text: str) -> Tuple or int:
     """
     if not isinstance(text, str):
         return -1
-    cleaned = []
-    for char in text.lower():
-        if char.isalpha() or char.isspace():
-            cleaned.append(char)
-    tokens = []
-    for word in ''.join(cleaned).split():
-        tokenized = [letter for letter in '_' + word + '_']
-        tokens.append(tuple(tokenized))
-    return tuple(tokens)
+    text = "".join(char for char in text if char.isalpha() or char.isspace())
+    tokenized = tuple(tuple("_" + word + "_") for word in text.lower().strip().split())
+    return tokenized
 
 
 # 4
@@ -40,9 +36,9 @@ class LetterStorage(Storage):
         """
         if not isinstance(elements, tuple):
             return -1
-        for element in elements:
-            for character in element:
-                self._put(character)
+        for token in elements:
+            for letter in token:
+                self._put(letter)
         return 0
 
     def get_letter_count(self) -> int:
@@ -51,8 +47,7 @@ class LetterStorage(Storage):
         """
         if not self.storage:
             return -1
-        size = len(self.storage)
-        return size
+        return len(self.storage)
 
 
 # 4
@@ -63,10 +58,10 @@ def encode_corpus(storage: LetterStorage, corpus: tuple) -> tuple:
     :param corpus: a tuple of tuples
     :return: a tuple of the encoded letters
     """
-    if not isinstance(storage, LetterStorage) or not isinstance(corpus, tuple):
+    if not (isinstance(storage, LetterStorage) and isinstance(corpus, tuple)):
         return ()
     storage.update(corpus)
-    encoded = tuple(tuple(storage.storage[letter] for letter in element) for element in corpus)
+    encoded = tuple(tuple(storage.get_id(letter) for letter in token) for token in corpus)
     return encoded
 
 
@@ -78,10 +73,11 @@ def decode_sentence(storage: LetterStorage, sentence: tuple) -> tuple:
     :param sentence: a tuple of tuples-encoded words
     :return: a tuple of the decoded sentence
     """
-    if not isinstance(storage, LetterStorage) or not isinstance(sentence, tuple):
+    if not (isinstance(storage, LetterStorage) and isinstance(sentence, tuple)):
         return ()
-    storage_inverted = {value: key for key, value in storage.storage.items()}
-    decoded = tuple(tuple(storage_inverted[character] for character in word) for word in sentence)
+    decoded = tuple(
+        tuple(storage.get_element(letter) for letter in token) for token in sentence
+    )
     return decoded
 
 
@@ -97,13 +93,14 @@ class NGramTextGenerator:
 
     def get_trie_by_level(self, trie_level: int):
         """
-        Gets NGramTrie of particular N-gram size
+        Gets NGramTrie of the requested N-gram size
         :param trie_level: N-gram size
-        :return: NGramTrie
+        :return: NGramTrie if succeeds, None if not
         """
         for trie in self.profile.tries:
             if trie.size == trie_level:
                 return trie
+        return None
 
     def _generate_letter(self, context: tuple) -> int:
         """
@@ -116,15 +113,24 @@ class NGramTextGenerator:
         trie = self.get_trie_by_level(len(context) + 1)
         if not trie:
             return -1
-        frequencies = {n_gram: freq for n_gram, freq in trie.n_gram_frequencies.items()
-                       if n_gram[:-1] == context and n_gram not in self._used_n_grams}
+        frequencies = {
+            n_gram: freq
+            for n_gram, freq in trie.n_gram_frequencies.items()
+            if n_gram[:-1] == context and n_gram not in self._used_n_grams
+        }
         if not frequencies:
-            frequencies = {n_gram: freq for n_gram, freq in trie.n_gram_frequencies.items()
-                           if n_gram not in self._used_n_grams}
+            frequencies = {
+                n_gram: freq
+                for n_gram, freq in trie.n_gram_frequencies.items()
+                if n_gram not in self._used_n_grams
+            }
         if not frequencies:
             self._used_n_grams = []
-            frequencies = {n_gram: freq for n_gram, freq in trie.n_gram_frequencies.items()
-                           if n_gram[:-1] == context}
+            frequencies = {
+                n_gram: freq
+                for n_gram, freq in trie.n_gram_frequencies.items()
+                if n_gram[:-1] == context
+            }
         if not frequencies:
             return -1
         n_gram = max(frequencies, key=frequencies.get)
@@ -137,27 +143,50 @@ class NGramTextGenerator:
         """
         if not isinstance(context, tuple) or not isinstance(word_max_length, int):
             return ()
+
+        special_id = self.profile.storage.get_special_token_id()
+        trie_level = len(context)
+        if context[-1] == special_id:
+            context = (special_id,)
         token = [*context]
         letter = None
-        print(self.profile.storage.storage)
-        while not (letter == self.profile.storage.get_special_token_id()
-                   or len(token) > word_max_length + 1):
+
+        while not (letter == special_id or len(token) >= word_max_length):
+            context = tuple(token[-trie_level:])
             letter = self._generate_letter(context)
-            context = *context[1:], letter
             token.append(letter)
-        return tuple(token)
+
+        if len(token) >= word_max_length and token[-1] != special_id:
+            token.append(special_id)
+        word = tuple(token)
+        return word
 
     def generate_sentence(self, context: tuple, word_limit: int) -> tuple:
         """
         Generates full sentence with fixed number of words given.
         """
-        pass
+        if not isinstance(context, tuple) or not isinstance(word_limit, int):
+            return ()
+        sentence = []
+        for _ in range(word_limit):
+            word = self._generate_word(context)
+            context = word[-len(context) :]
+            sentence.append(word)
+        sentence = tuple(sentence)
+        return sentence
 
     def generate_decoded_sentence(self, context: tuple, word_limit: int) -> str:
         """
         Generates full sentence and decodes it
         """
-        pass
+        if not isinstance(context, tuple) or not isinstance(word_limit, int):
+            return ()
+        sentence = self.generate_sentence(context, word_limit)
+        sentence = tuple(
+            self.profile.storage.get_element(i) for token in sentence for i in token
+        )
+        generation = translate_sentence_to_plain_text(sentence)
+        return generation
 
 
 # 6
@@ -165,7 +194,18 @@ def translate_sentence_to_plain_text(decoded_corpus: tuple) -> str:
     """
     Converts decoded sentence into the string sequence
     """
-    pass
+    if not isinstance(decoded_corpus, tuple):
+        return ""
+    sentence = [letter for token in decoded_corpus for letter in token]
+    if len(sentence):
+        if sentence[0] == "_":
+            sentence.pop(0)
+        if sentence[0].islower():
+            sentence[0] = sentence[0].upper()
+        if sentence[-1] == "_":
+            sentence[-1] = "."
+    sentence = re.sub("_+", " ", "".join(sentence))
+    return sentence
 
 
 # 8
@@ -181,7 +221,19 @@ class LikelihoodBasedTextGenerator(NGramTextGenerator):
         :param context: a context for the letter given
         :return: float number, that indicates maximum likelihood
         """
-        pass
+        if not (isinstance(letter, int) and isinstance(context, tuple) and context):
+            return -1
+        n_gram = (*context, letter)
+        trie = self.get_trie_by_level(len(context) + 1)
+        contexts = {
+            n_gram: freq
+            for n_gram, freq in trie.n_gram_frequencies.items()
+            if n_gram[:-1] == context
+        }
+        if not (contexts and n_gram in contexts):
+            return 0.0
+        probability = contexts[n_gram] / sum(contexts.values())
+        return probability
 
     def _generate_letter(self, context: tuple) -> int:
         """
@@ -189,7 +241,24 @@ class LikelihoodBasedTextGenerator(NGramTextGenerator):
             Takes the letter with highest
             maximum likelihood frequency.
         """
-        pass
+        if not (isinstance(context, tuple) and context):
+            return -1
+        trie = self.get_trie_by_level(len(context) + 1)
+        probabilities = {}
+        contexts = {
+            n_gram: freq
+            for n_gram, freq in trie.n_gram_frequencies.items()
+            if n_gram[:-1] == context
+        }
+        if not contexts:
+            trie = self.get_trie_by_level(1)
+            contexts = trie.n_gram_frequencies
+        for n_gram in contexts:
+            probabilities[n_gram] = self._calculate_maximum_likelihood(
+                n_gram[-1], n_gram[:-1]
+            )
+        letter = max(probabilities, key=probabilities.get)[-1]
+        return letter
 
 
 # 10
@@ -205,7 +274,19 @@ class BackOffGenerator(NGramTextGenerator):
             available frequency for the corresponding context.
             if no context can be found, reduces the context size by 1.
         """
-        pass
+        if not isinstance(context, tuple):
+            return -1
+        trie = self.get_trie_by_level(len(context) + 1)
+        contexts = {
+            n_gram: freq
+            for n_gram, freq in trie.n_gram_frequencies.items()
+            if n_gram[:-1] == context and n_gram not in self._used_n_grams
+        }
+        if not contexts:
+            return self._generate_letter(context[1:])
+        n_gram = max(contexts, key=contexts.get)
+        self._used_n_grams.append(n_gram)
+        return n_gram[-1]
 
 
 # 10
@@ -219,4 +300,34 @@ class PublicLanguageProfile(LanguageProfile):
         Opens public profile and adapts it.
         :return: o if succeeds, 1 otherwise
         """
-        pass
+        if not isinstance(file_name, str):
+            return 1
+        with open(file_name, encoding="utf-8") as file:
+            text = json.load(file)
+        self.language = text["name"]
+        self.n_words = text["n_words"]
+        self.tries = [
+            self._build_n_gram_trie(text["freq"], trie_level)
+            for trie_level in set(map(len, text["freq"]))
+        ]
+        return 0
+
+    def _build_n_gram_trie(
+        self, n_gram_frequencies: dict, trie_level: int
+    ) -> NGramTrie:
+        """
+        Helper function to build N-Gram tries from imported dictionaries.
+        :param n_gram_frequencies: a dictionary of N-gram frequencies
+        :param trie_level: trie level
+        :return: NGramTrie
+        """
+        n_gram_trie = NGramTrie(trie_level, self.storage)
+        for raw_n_gram, freq in n_gram_frequencies.items():
+            if len(raw_n_gram) == trie_level:
+                n_gram = raw_n_gram.replace(" ", "_").lower()
+                self.storage.update(tuple(n_gram))
+                n_gram = tuple(map(self.storage.get_id, n_gram))
+                if n_gram not in n_gram_trie.n_gram_frequencies:
+                    n_gram_trie.n_gram_frequencies[n_gram] = 0
+                n_gram_trie.n_gram_frequencies[n_gram] += freq
+        return n_gram_trie
--- lab_4/start.py
+++ lab_4/start.py
@@ -3,14 +3,67 @@ Language generation starter
 """
 
 import os
+from lab_4.main import (
+    tokenize_by_letters,
+    LetterStorage,
+    encode_corpus,
+    LanguageProfile,
+    NGramTextGenerator,
+    LikelihoodBasedTextGenerator,
+    BackOffGenerator,
+    PublicLanguageProfile,
+)
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    # find the appropriate start.py task in your lab_4 description file
-    # your code goes here
+    with open(
+        os.path.join(PATH_TO_LAB_FOLDER, "reference_text.txt"), encoding="utf-8"
+    ) as f:
+        corpus = tokenize_by_letters(f.read())
+    # score 4
+    storage = LetterStorage()
+    storage.update(corpus)
+    print(len(storage.storage))
+    print(list(storage.storage.items())[:5])
+    print(list(storage.storage.items())[-5:])
 
-    RESULT = ''
+    # score 6
+    profile = LanguageProfile(storage, "idk")
+    profile.create_from_tokens(
+        encode_corpus(storage, corpus),
+        (
+            1,
+            2,
+        ),
+    )
+    generator = NGramTextGenerator(profile)
+    for length in range(5, 11):
+        print(generator.generate_decoded_sentence((1,), length))
+
+    # score 8
+    generator = LikelihoodBasedTextGenerator(profile)
+    for length in range(5, 11):
+        print(generator.generate_decoded_sentence((1,), length))
+
+    # score 10
+    profile = PublicLanguageProfile(LetterStorage(), "ne")
+    profile.open(os.path.join(PATH_TO_LAB_FOLDER, "ne"))
+
+    generation = []
+    for generator in [
+        NGramTextGenerator(profile),
+        LikelihoodBasedTextGenerator(profile),
+        BackOffGenerator(profile),
+    ]:
+        generation.append(
+            generator.generate_decoded_sentence(
+                (profile.storage.get_special_token_id(),), 5
+            )
+        )
+    print(" ".join(generation))
+
+    RESULT = generation
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT, 'Detection not working'
+    assert RESULT == generation, "Detection not working"
